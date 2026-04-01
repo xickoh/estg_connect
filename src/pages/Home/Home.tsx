acceca4261e3184755899a31bb14337cb3c51e2f
Nuno Gomes <nunocrgomes@gmail.com>
2026-04-01 22:23:10 +0100

    pequenos fixes nas parcerias

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { FC } from "react";
 import Hero from "../../components/hero/Hero";
 import Countdown from "../../components/countdown/Countdown";
+import Parcerias from "../../components/parcerias/Parcerias";
 
 const Home: FC = () => {
     return (
         <div id="home">
             <Hero />;
             <Countdown />
+            <Parcerias />
         </div>
     );
 };
