5801d836534e2b06d48a6d27dec00e7664ea1138
Leonardo <leolouresende@gmail.com>
2026-04-02 10:37:30 +0100

    Removidos imports inuteis
    Tinha import para navbar que não é utilizado visto que a navbar é um componente global da aplicação

--- src/pages/Faq.tsx
+++ src/pages/Faq.tsx
@@ -1,15 +1,10 @@
 import React from "react";
-import { FC } from "react";
 
 import FaqSection from "../components/faqSection/FaqSection";
-
-import Navbar from "../components/navbar/Navbar";
-//import Footer from "../components/Footer";
 
 const Faq = () => {
   return (
     <>
-      <Navbar />
       <FaqSection />
     </>
   );
