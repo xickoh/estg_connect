070005df41bfdb2c8e5c8e1b39106b808d2bcd19
Nuno Gomes <nunocrgomes@gmail.com>
2026-04-03 22:58:26 +0100

    Componente Inscrições

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { FC } from "react";
 import Hero from "../../components/hero/Hero";
 import Countdown from "../../components/countdown/Countdown";
+import Registrations from "../../components/registrations/Registrations";
 
 const Home: FC = () => {
     return (
         <div id="home">
             <Hero />;
             <Countdown />
+            <Registrations />
         </div>
     );
 };
